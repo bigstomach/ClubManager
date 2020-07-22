8a9bce47fdbcc9cb172a78784e5ebe69d0dd14ee
1851906 <51292030+1851906@users.noreply.github.com>
2020-07-22 23:31:10 +0800

    Update StudentService.cs

--- ClubManager/Services/StudentService.cs
+++ ClubManager/Services/StudentService.cs
@@ -14,11 +14,44 @@ namespace ClubManager.Services
             _context = context;
         }
 
+         //根据社团id 返回社团
+        private Clubs GetRelatedClub(long id)
+        {
+            return _context.Clubs.FirstOrDefault(c => c.ClubId == id);
+        }
+
+        //根据社团id 获取社团简介
+        public string GetClubDescription(long ClubId)
+        {
+            return GetRelatedClub(ClubId).Description;
+        }
+        
+        //返回所有社团
+        public IQueryable<ClubVO> GetClubInfo(string query)
+        {
+            var clubs = _context.Clubs
+               .Select(s => new ClubVO
+               {
+                   ClubId = s.ClubId,
+                   Name = s.Name,
+                   Description = s.Description,
+                   EstablishmentDate = s.EstablishmentDate,
+                   PresidentName = s.Student.Name,
+                   Type = s.Type,
+               });
+            if (!String.IsNullOrEmpty(query))
+            {
+                clubs = clubs.Where(s => s.Name.Contains(query));
+            }
+            return clubs;
+          }
+
+        //返回已加入社团
         public IQueryable<ClubVO> SearchInClub(long id, string query, bool status)
         {
-            var clubs = from stu in _context.Students
-                join joinclub in _context.JoinClubs on stu equals joinclub.Student
-                join club in _context.Clubs on joinclub.Club equals club
+            var clubs = from stu in _context.Set<Students>()
+                join joinclub in _context.Set<JoinClubs>() on stu equals joinclub.Student
+                join club in _context.Set<Clubs>() on joinclub.Club equals club
                 where stu.UserId == id && joinclub.Status == status
                 select new ClubVO
                 {
@@ -37,7 +70,7 @@ namespace ClubManager.Services
 
             return clubs;
         }
-
+         //返回学生姓名
         public string GetStudentName(long userId)
         {
             return _context.Students.First(s => s.UserId == userId).Name;
