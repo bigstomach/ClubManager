f34abae54fa9285b6addf87e63f1075a7dd990b4
1851906 <51292030+1851906@users.noreply.github.com>
2020-09-09 21:22:00 +0800

    社长名字查询用到

---
+++ ClubManager/ViewObjects/PresidentVO.cs
@@ -0,0 +1,9 @@
+namespace ClubManager.ViewObjects
+{
+    //社团现任社长查询视图
+    public class PresidentVO
+    {
+        public long ClubId { get; set; }
+        public string Name { get; set; }//现任社长的名字
+    }
+}
