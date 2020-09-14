dab4aee637bd60c467078286ed04c60ffc131179
Lijinqizuishuai <49816660+Lijinqizuishuai@users.noreply.github.com>
2020-09-15 00:15:07 +0800

    删掉clubID
    删掉clubID

--- ClubManager/QueryObjects/JoinClubStatusQO.cs
+++ ClubManager/QueryObjects/JoinClubStatusQO.cs
@@ -9,7 +9,7 @@ namespace ClubManager.QueryObjects
     {
         public long StudentId { get; set; }
 
-        public long ClubId { get; set; }
+        
         public bool Status { get; set; }//false 待审核和拒绝   true 加入
     }
 }
